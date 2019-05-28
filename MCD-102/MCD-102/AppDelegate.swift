2921a6229897b03208a9cee64662d8aaea9bb5b5
Azmi <yaffiazmi.dev@gmail.com>
2019-05-28 08:53:34 +0700

    add starter project

---
+++ MCD-102/MCD-102/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MCD-102
+//
+//  Created by yaffi azmi on 28/05/19.
+//  Copyright © 2019 yaffi azmi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: LoginViewController())
+        
+        return true
+    }
+}
+
---
+++ MCD-102/MCD-102/Classes/Modules/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  MCD-101
+//
+//  Created by yaffi azmi on 28/05/19.
+//  Copyright © 2019 yaffi azmi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
